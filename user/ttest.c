6b62588dd2380c650e4a74135d00f2d41a07938c
bennybru <bennybrud@gmail.com>
2021-05-10 13:12:43 +0300

    ttest update to test kthread_join

--- user/ttest.c
+++ user/ttest.c
@@ -13,23 +13,47 @@
 // #include "Csemaphore.h"   // NEW INCLUDE FOR ASS 2
 #include "../kernel/proc.h"         // NEW INCLUDE FOR ASS 2, has all the signal definitions and sigaction definition.  Alternatively, copy the relevant things into user.h and include only it, and then no need to include spinlock.h .
 
+int i;
+
 void test_thread(){
-    printf("Thread is now running\n");
+    if (i == 1) {
+        printf("error/n");
+    }
     kthread_exit(0);
 }
 
+// void test_thread(){
+//     printf("Thread is now running\n");
+//     kthread_exit(0);
+// }
+
 void thread_test(){
+    i = 0;
     int tid;
     int status;
     void* stack = malloc(MAX_STACK_SIZE);
     tid = kthread_create(test_thread, stack);
-    printf("Finished creating threads\n");
     kthread_join(tid,&status);
+
+    i++;
 
     tid = kthread_id();
     free(stack);
     printf("Finished testing threads, main thread id: %d, %d\n", tid,status);
 }
+
+// void thread_test(){
+//     int tid;
+//     int status;
+//     void* stack = malloc(MAX_STACK_SIZE);
+//     tid = kthread_create(test_thread, stack);
+//     printf("Finished creating threads\n");
+//     kthread_join(tid,&status);
+
+//     tid = kthread_id();
+//     free(stack);
+//     printf("Finished testing threads, main thread id: %d, %d\n", tid,status);
+// }
 
 int main(int argc, char **argv){
     thread_test();
